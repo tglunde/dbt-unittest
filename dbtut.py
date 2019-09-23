c8f145d5efa58b85760bb41f44462fada13b7d25
ilikutle <ilijakutle@gmail.com>
2019-09-23 15:36:51 +0200

    added doc paths, changed program structure
    added new func for getting test and dataset files paths in dictionary, old main func became insert_data func, changed some variable names to follow pep8 naming rules

--- dbtut.py
+++ dbtut.py
@@ -1,33 +1,59 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 import sqlalchemy
+import os
+import pathlib
 
-def main():
-    document_path = "./unittest/dataset/customerlayer_functional_testset_campaign_r.xml"
-    testdata = {}
+
+def get_dataset():
+    directory = pathlib.Path("./unittest")
+    tests = {}
+    for row in [x for x in directory.iterdir() if x.is_dir()]:
+        tests_table = []
+        if row.name not in tests:
+            tests[row.name] = tests_table
+        else:
+            tests_table = tests[row.name]
+        for docs in [y for y in row.iterdir()]:
+            tests_table.append(docs)
+    return tests
+
+
+def insert_data(document_path):
+    test_data = {}
     for event, element in ET.iterparse(document_path, events=('start', 'end')):
         if event == 'start' and element.tag!='dataset':
-            testtable = []
-            if element.tag not in testdata:
-                testdata[element.tag] = testtable
+            test_table = []
+            if element.tag not in test_data:
+                test_data[element.tag] = test_table
             else:
-                testtable = testdata[element.tag]
-            testrow = {}
-            testtable.append(testrow)
+                test_table = test_data[element.tag]
+            test_row = {}
+            test_table.append(test_row)
             for attribute in element.attrib.items():
-                testrow[attribute[0]] = attribute[1]
+                test_row[attribute[0]] = attribute[1]
 
     db = sqlalchemy.create_engine('postgresql://postgres@localhost/postgres')
-    for tablename in testdata.keys():
+    for table_name in test_data.keys():
         df = pd.DataFrame.from_records(
-            testdata[tablename], 
-            columns=testdata[tablename][0].keys())
-        schema_tablename = tablename.split('.', 1)
-        if not db.dialect.has_schema(db, schema_tablename[0]):
-            db.execute(sqlalchemy.schema.CreateSchema(schema_tablename[0]))
-        df.to_sql(schema_tablename[1], db, schema=schema_tablename[0], if_exists='replace', index=False)
+            test_data[table_name], 
+            columns=test_data[table_name][0].keys())
+        schema_table_name = table_name.split('.', 1)
+        if not db.dialect.has_schema(db, schema_table_name[0]):
+            db.execute(sqlalchemy.schema.CreateSchema(schema_table_name[0]))
+        df.to_sql(schema_table_name[1], db, schema=schema_table_name[0], if_exists='replace', index=False)
     print("{} inserted!".format(pathlib.PurePosixPath(document_path).name))
-    
+
+
+def main():
+    dataset = get_dataset()
+
+    for key, value in dataset.items():
+        if key == 'dataset':
+            for doc in value:
+                if doc.name.endswith('.xml'):
+                    insert_data(doc)
+
 
 if __name__ == "__main__":
     main()
